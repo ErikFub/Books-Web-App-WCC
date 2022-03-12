c51d54f287b45253eb3108d6e42f728fe9bf7b91
sunnihermann <101108231+sunnihermann@users.noreply.github.com>
2022-03-12 20:24:32 +0000

    AddBooks update

--- public/script.js
+++ public/script.js
@@ -201,12 +201,7 @@ function search(){
 
 // Add another author -----------------------------------------------------------
 function createNewElement() {
-    // First create a DIV element.
 	var txtNewInputBox = document.createElement('div');
-
-    // Then add the content (a new input box) of the element.
 	txtNewInputBox.innerHTML = "<input type='text' id='newInputBox'>";
-
-    // Finally put it where it is supposed to appear.
 	document.getElementById("newElementId").appendChild(txtNewInputBox);
 }
