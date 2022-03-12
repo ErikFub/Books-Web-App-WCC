6dc1a980c31a137a2a7c79d9735d91ca0aad5d02
sunnihermann <101108231+sunnihermann@users.noreply.github.com>
2022-03-12 19:08:41 +0000

    Add Book css & html draft

--- public/script.js
+++ public/script.js
@@ -197,3 +197,16 @@ function search(){
         .then(books => filterBooks(books, searchInput))
         .then(filteredBooks => fillBooks(filteredBooks))
 }
+
+
+// Add another author -----------------------------------------------------------
+function createNewElement() {
+    // First create a DIV element.
+	var txtNewInputBox = document.createElement('div');
+
+    // Then add the content (a new input box) of the element.
+	txtNewInputBox.innerHTML = "<input type='text' id='newInputBox'>";
+
+    // Finally put it where it is supposed to appear.
+	document.getElementById("newElementId").appendChild(txtNewInputBox);
+}
