6cf548abb32d0c75a8742276490ad13a464e8939
flaviasironi <101108122+flaviasironi@users.noreply.github.com>
2022-03-13 12:37:59 +0000

    Statistics page draft

---
+++ public/statistics.js
@@ -0,0 +1,9 @@
+// function getCount(group) {
+//     var count = 0;
+//     for (var i = 0; i < obj.data.length; i++) {
+//         if (obj.data[i].rating == rating) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
